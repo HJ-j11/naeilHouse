f0ec49a7da12b518a08e738a6196bdffae05f7ae
SJP03 <qkrtjdwns03@gmail.com>
2022-03-30 01:02:09 +0900

    메인화면 추가
    index.html 추가
    TestController 테스트

--- start/start/src/main/java/com/house/start/controller/TestController.java
+++ start/start/src/main/java/com/house/start/controller/TestController.java
@@ -1,5 +1,15 @@
 package com.house.start.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class TestController {
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        return "sample";
+    }
+
 }
