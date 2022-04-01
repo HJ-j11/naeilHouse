0ce59a1c1d55de878d3902bf1b863276a65bfe0a
chea-young <63410509+chea-young@users.noreply.github.com>
2022-04-02 00:08:59 +0900

    feat: add admin test file

--- start/start/src/main/java/com/house/start/repository/AdminRepository.java
+++ start/start/src/main/java/com/house/start/repository/AdminRepository.java
@@ -1,26 +1,28 @@
 package com.house.start.repository;
 
 import com.house.start.domain.Admin;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.Optional;
 
-public class JpaAdminRepository {
+@Repository
+public class AdminRepository {
     
     private final EntityManager em;
     
-    public JpaAdminRepository(EntityManager em) {
+    public AdminRepository(EntityManager em) {
         this.em = em;
     }
     
-    public Admin save(Admin admin) {
+    public Long save(Admin admin) {
         em.persist(admin);
-        return admin;
+        return admin.getAdmin_id();
     }
 
     // 로그인 시, pwd
     // 로그인 후, name
-    public Optional<Admin> findByAdmin_id(String admin_id) {
+    public Optional<Admin> findByAdmin_id(Long admin_id) {
         Admin admin = em.find(Admin.class, admin_id);
         return Optional.ofNullable(admin);
     }
---
+++ start/start/src/test/java/com/house/start/repository/AdminRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.house.start.repository;
+
+import com.house.start.domain.Admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminRepositoryTest {
+    @Autowired AdminRepository adminRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testAdmin() {
+        Admin admin = new Admin();
+        admin.setId("admin");
+        admin.setName("admin");
+        admin.setPwd("1234");
+        Long savedId = adminRepository.save(admin);
+
+        Optional<Admin> findAdmin = adminRepository.findByAdmin_id(savedId);
+    }
+}
