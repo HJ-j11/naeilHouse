f1f94a02332455d11ef55fa5a013da92b386c74a
chea-young <63410509+chea-young@users.noreply.github.com>
2022-04-02 16:56:50 +0900

    fix: modify adminrepositorytest error

--- start/start/src/main/java/com/house/start/repository/TestRepository.java
+++ start/start/src/main/java/com/house/start/repository/TestRepository.java
@@ -1,6 +1,6 @@
 package com.house.start.repository;
 
-import com.house.start.entity.Item;
+import com.house.start.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- start/start/src/test/java/com/house/start/repository/AdminRepositoryTest.java
+++ start/start/src/test/java/com/house/start/repository/AdminRepositoryTest.java
@@ -15,7 +15,6 @@ import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@WebAppConfiguration
 public class AdminRepositoryTest {
     @Autowired AdminRepository adminRepository;
 
