eeb41b03e0f860499a11b59e02036bf63920b7ff
chea-young <63410509+chea-young@users.noreply.github.com>
2022-04-09 23:43:36 +0900

    fix: change getter, setter in admin entity

--- start/start/src/main/java/com/house/start/domain/Admin.java
+++ start/start/src/main/java/com/house/start/domain/Admin.java
@@ -1,12 +1,15 @@
 package com.house.start.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 public class Admin {
     @Id
+    @Getter @Setter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "admin_id")
     private Long admin_id;
 
     private String name;
@@ -14,37 +17,5 @@ public class Admin {
     private String id;
 
     private String pwd;
-
-    public Long getAdmin_id() {
-        return admin_id;
-    }
-
-    public void setAdmin_id(Long admin_id) {
-        this.admin_id = admin_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId(){
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPwd(){
-        return pwd;
-    }
-
-    public void setPwd (String pwd) {
-        this.pwd = pwd;
-    }
 
 }
