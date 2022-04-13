7acc93dbfc9fee25d8d0f213618cbb508dfb8654
HJ <hyejiku98@gmail.com>
2022-04-13 16:09:39 +0900

    temporal

--- start/start/src/main/java/com/house/start/repository/ConsumerRepository.java
+++ start/start/src/main/java/com/house/start/repository/ConsumerRepository.java
@@ -4,7 +4,6 @@ import com.house.start.domain.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
 
 @Repository
 public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
