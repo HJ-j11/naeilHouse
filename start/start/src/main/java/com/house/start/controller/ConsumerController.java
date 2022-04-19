6aeea4c421ba6d56b19b27371b529f82d03617b8
HJ <hyejiku98@gmail.com>
2022-04-19 14:36:36 +0900

    댓글 기능 - 댓글 작성 - 댓글 작성 후 thymeleaf로 보여줌.

--- start/start/src/main/java/com/house/start/controller/ConsumerController.java
+++ start/start/src/main/java/com/house/start/controller/ConsumerController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
