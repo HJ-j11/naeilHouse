9ae6b26798694a36b14f6efc67668f9060596a03
SJP03 <qkrtjdwns03@gmail.com>
2022-04-27 22:01:25 +0900

    회원가입 기능 추가

---
+++ start/start/src/main/java/com/house/start/controller/JoinController.java
@@ -0,0 +1,116 @@
+package com.house.start.controller;
+
+import com.house.start.controller.form.MemberJoinForm;
+import com.house.start.domain.Admin;
+import com.house.start.domain.Consumer;
+import com.house.start.domain.Seller;
+import com.house.start.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    /**
+     *  어느 역할(소비자 or 판매자 or 관리자)로 회원가입 할지 고르는 페이지
+     */
+    @GetMapping("/member/join")
+    public String joinSelect() {
+        return "join/member_select";
+    }
+
+    /**
+     *  소비자 회원가입 등록 폼 페이지
+     */
+    @GetMapping("/consumer/join")
+    public String joinConsumer(Model model) {
+        model.addAttribute("form", new MemberJoinForm());
+        return "join/consumer_add";
+    }
+
+    /**
+     *  소비자 회원가입 등록
+     */
+    @PostMapping("/consumer/join")
+    public String joinConsumer(@ModelAttribute MemberJoinForm form) {
+
+        // 소비자 객체 생성
+        Consumer consumer = new Consumer();
+        consumer.setCId(form.getId());
+        consumer.setPwd(form.getPassword());
+        consumer.setName(form.getName());
+
+        // 포인트 초기화
+        consumer.setPoint(500000);
+
+        joinService.joinConsumer(consumer);
+
+        return "redirect:/";
+    }
+
+    /**
+     *  판매자 회원가입 등록 폼 페이지
+     */
+    @GetMapping("/seller/join")
+    public String joinSeller(Model model) {
+        model.addAttribute("form", new MemberJoinForm());
+        return "join/seller_add";
+    }
+
+    /**
+     *  판매자 회원가입 등록
+     */
+    @PostMapping("/seller/join")
+    public String joinSeller(@ModelAttribute MemberJoinForm form) {
+
+        // 판매자 객체 생성
+        Seller seller = new Seller();
+        seller.setSId(form.getId());
+        seller.setPwd(form.getPassword());
+        seller.setName(form.getName());
+        seller.setStoreName(form.getStoreName());
+
+        // 회원 신청 승인여부 false
+        seller.setIsApproved(false);
+
+        joinService.joinSeller(seller);
+
+        return "redirect:/";
+    }
+
+    /**
+     *  관리자 회원가입 등록 폼 페이지
+     */
+    @GetMapping("/admin/join")
+    public String joinAdmin(Model model) {
+        model.addAttribute("form", new MemberJoinForm());
+        return "join/admin_add";
+    }
+
+    /**
+     *  관리자 회원가입 등록
+     */
+    @PostMapping("/admin/join")
+    public String joinAdmin(@ModelAttribute MemberJoinForm form) {
+
+        // 관리자 객체 생성
+        Admin admin = new Admin();
+        admin.setAId(form.getId());
+        admin.setPwd(form.getPassword());
+        admin.setName(form.getName());
+
+        joinService.joinAdmin(admin);
+
+        return "redirect:/";
+    }
+
+}
