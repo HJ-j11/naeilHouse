0e827fcc7345344638e2694bb6b1e8dcb5bec6ea
chea-young <63410509+chea-young@users.noreply.github.com>
2022-05-10 23:21:50 +0900

    fix: modify 'admin routing;

---
+++ start/start/src/main/java/com/house/start/controller/Consumer/ConsumerMypageController.java
@@ -0,0 +1,13 @@
+package com.house.start.controller.Consumer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("user")
+public class ConsumerMypageController {
+}
