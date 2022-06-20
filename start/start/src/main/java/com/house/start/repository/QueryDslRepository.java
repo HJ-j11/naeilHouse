19a4dbec3b5d217a88174e50f11df11ca691fd8a
HJ <hyejiku98@gmail.com>
2022-06-20 16:14:20 +0900

    feat : QueryDSL example QueryDslRepository added

---
+++ start/start/src/main/java/com/house/start/repository/QueryDslRepository.java
@@ -0,0 +1,24 @@
+package com.house.start.repository;
+
+import static com.house.start.domain.QPost.*;
+import static com.house.start.domain.QItem.*;
+
+import com.house.start.domain.Post;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class QueryDslRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public List<Post> findPostsByContent(String word) {
+        return jpaQueryFactory.select(post)
+                .where(post.contents.like(word))
+                .fetch();
+    }
+
+}
---
+++ start/start/src/test/java/com/house/start/service/SearchServiceTest.java
@@ -0,0 +1,22 @@
+package com.house.start.service;
+
+import com.house.start.domain.Post;
+import com.house.start.domain.QPost;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class SearchServiceTest {
+    @Test
+    void 단어_검색() {
+
+    }
+}
