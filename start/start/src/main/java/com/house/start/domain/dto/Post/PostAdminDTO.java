919e9aad5231f78c1db413e86ff3d0bec5341767
chea-young <63410509+chea-young@users.noreply.github.com>
2022-07-01 22:34:02 +0900

    fix: change session part

--- start/start/src/main/java/com/house/start/domain/dto/Post/PostAdminDTO.java
+++ start/start/src/main/java/com/house/start/domain/dto/Post/PostAdminDTO.java
@@ -1,9 +1,30 @@
 package com.house.start.domain.dto.Post;
 
+import com.house.start.domain.Post;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 public class PostAdminDTO {
+    private Long id;
+    private String cId;
+    private LocalDateTime postDate;
+    private String contents;
+    private int like_size;
+    private int comments_size;
+    private String uploadFileName;
+
+    public PostAdminDTO (Post post) {
+        this.id = post.getId();
+        this.cId = post.getConsumer().getCId();
+        this.postDate = post.getPostDate();
+        this.contents = post.getContents();
+        this.like_size = post.getLikes().size();
+        this.comments_size = post.getComments().size();
+        this.uploadFileName = post.getUploadFile().getUploadFileName();
+    }
 }
