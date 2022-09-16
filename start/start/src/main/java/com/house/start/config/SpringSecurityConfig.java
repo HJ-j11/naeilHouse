12842a9821630a4692faa2d0cb434c7f1d17c146
HJ <hyejiku98@gmail.com>
2022-09-16 14:17:36 +0900

    fix : csrf disabled & navbar 링크 추가

--- start/start/src/main/java/com/house/start/config/SpringSecurityConfig.java
+++ start/start/src/main/java/com/house/start/config/SpringSecurityConfig.java
@@ -84,6 +84,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected  void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated()
         .and()
