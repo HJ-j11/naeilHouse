309b9944c763673cb93ab8e4e4d50632c7cf8b47
chea-young <63410509+chea-young@users.noreply.github.com>
2022-09-29 00:46:02 +0900

    Update: update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ sudo docker rmi lchy0413/naeilhouse-server:1.0
 sudo docker pull lchy0413/naeilhouse-server:1.0
 
 # 도커 run
-docker run -d -p 8080:8080 --name naeilhouse-server lchy0413/naeilhouse-server:1.0
+docker run -d -p 8080:8080 -v /home/ec2-user:/config --name naeilhouse-server lchy0413/naeilhouse-server:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
